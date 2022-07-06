5b44dd130601b5bada557b6a811bf111fd17a1e4
sho watanabe <showatanabe0226@gmail.com>
2022-07-06 18:57:29 +0900

    first commit

---
+++ src/components/PersonList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Person } from "./Person.vue";
+type Props = {
+  persons: Person[];
+};
+
+defineProps<Props>();
+
+const emit = defineEmits(["delete"]);
+
+const onClickDelete = (id: number) => {
+  emit("delete", id);
+};
+</script>
+<template>
+  <li v-for="person in persons" :key="person.id" class="person-list">
+    <span>{{ person.name }}</span>
+    <span>age:{{ person.age }}</span>
+    <button v-on:click="onClickDelete(person.id)">
+      <span>delete</span>
+    </button>
+  </li>
+</template>
+<style scoped>
+.person-list {
+  list-style: none;
+  margin-bottom: 12px;
+  border-radius: 4px;
+  font-size: 20px;
+  display: flex;
+  justify-content: space-between;
+  background-color: blanchedalmond;
+  padding: 8px 20px;
+  width: 300px;
+}
+</style>
